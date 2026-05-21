248aa29b9fc8982198c65bb6f8c020a06a7e2b7b
Sam Breed <samuel.breed@gmail.com>
2026-05-20 23:25:07 -0700

    feat: export splitProseAndDiffs and related types from WalkthroughView
    Agent-Id: agent-f16695b0-b820-4819-b6ee-e0fe8b7ff493

--- packages/app/src/renderer/src/components/WalkthroughView/index.ts
+++ packages/app/src/renderer/src/components/WalkthroughView/index.ts
@@ -2,6 +2,9 @@ export { WalkthroughView } from "./WalkthroughView";
 export {
   buildWalkthroughSections,
   extractWalkthroughHeadings,
+  splitProseAndDiffs,
+  type DiffWithLabel,
+  type SplitSection,
   type WalkthroughHeading,
   type WalkthroughSection,
 } from "./walkthrough-parser";
